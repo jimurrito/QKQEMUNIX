91e62b7265b8a8e86529ec8e3eb593e0900a0cd7
jimurrito <jimurrito@gmail.com>
2026-05-04 22:25:35 -0500

    optimized how port forwarding is setup

--- flake.nix
+++ flake.nix
@@ -179,14 +179,14 @@
                       environment =
                         let
                           #conf.portForward.vmPort;
-                          NET_OPTS = #mkMerge (
+                          NET_OPTS = mkMerge (
                             mapAttrsToList (_: ports: 
                               "hostfwd=tcp::${toString ports.vm}-:${toString ports.host}"
-                            ) conf.portForwarding;
-                          #);
+                            ) conf.portForwarding
+                          );
                         in
                         {
-                          QEMU_NET_OPTS = concatStringsSep "," NET_OPTS;
+                          #QEMU_NET_OPTS = concatStringsSep "," NET_OPTS;
                         };
                       serviceConfig = {
                         User = "qkqemunix";
